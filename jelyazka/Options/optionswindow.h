fa9306615d144f311c88ab8c4a0b30653641e28a
Vladimir Stoyanov <firojjsci@gmail.com>
2018-12-15 11:40:16 +0200

    Updated optionwindows

--- jelyazka/Options/optionswindow.h
+++ jelyazka/Options/optionswindow.h
@@ -19,21 +19,21 @@
 #ifndef OPTIONSWINDOW_H
 #define OPTIONSWINDOW_H
 
-
-#include <QWidget>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <QCheckBox>
+#include <QLineEdit>
 #include <QListWidget>
 #include <QResizeEvent>
-#include <QThreadPool>
-#include <QTextEdit>
-#include <QCheckBox>
 #include <QSpinBox>
-#include <QLineEdit>
-#include <boost/ptr_container/ptr_vector.hpp>
-#include "rssthread.h"
+#include <QTextEdit>
+#include <QThreadPool>
+#include <QWidget>
+
+#include "data.h"
+#include "database.h"
 #include "mainwindow.h"
 #include "http.h"
-#include "database.h"
-#include "data.h"
+#include "rssthread.h"
 
 namespace Ui {
 class OptionsWindow;
@@ -53,19 +53,74 @@ public slots:
     void onFinish(QString name, bool finish);
 
 private slots:
+    //ToDo: rename button names
     void on_pushButton_clicked(); //'Remove' button clicked
     void on_pushButton_2_clicked(); // '<<' button clicked
     void on_OK_Button_clicked(); //'OK' button clicked
     void on_Cancel_Button_clicked(); //'Cancel' button clicked
     void on_pushButton_3_clicked(); // '>>' button clicked
     void on_collect_feeds_DoubleClicked(QListWidgetItem*);
-
     void on_treeWidget_clicked(const QModelIndex &index);
     void on_cb_enable_proxy_clicked(bool);
     void on_cb_enable_filtering_clicked(bool);
     void on_pb_add_filter_clicked();
     void on_pb_remove_filter();
     void on_textChanged(QString text);
+
+private:
+     void resizeEvent(QResizeEvent *event);
+     void showEvent(QShowEvent *);
+private:
+    void addItem(QString name);
+    void collect_feedsTableUpdate(); //ToDo: change the funtion name
+    void fillCollectListView();
+    void fillFilterListView();
+    void fillViewListView();
+    void insertRowToRSSTable(QString site_name, QString url, QString version);
+    void insertRowToFiltersTable(QString filter_name);
+    void rssTableUpdate();
+    void removeDataFromCollectFeeds(QString site_name);
+    void removeDataFromFilters(QString filter_name, bool all_data);
+    void removeDataFromRSSTable(QString site_name, bool all_data);
+    void returnModifedString(QString &str); //ToDo: change the function name
+    void updateCollectFeedListView();
+    void updateFiltersTable();
+private:
+    void loadOptions();
+    void saveOptions();
+
+private:
+    //hide widgets
+    void hideCollectionFeeds();
+    void hideFilters();
+    void hideNotifications();
+    void hideProxyConnection();
+    //show widgets
+    void showCollectionFeeds();
+    void showFilters();
+    void showNotifications();
+    void showProxyConnection();
+
+private:
+    int addStringToFilterList(QString cur_text);
+    int addStringToWatchList(QString str);
+    int addStringToViewList(QString str);
+    int cf_label_search_width(); //ToDo: change the function name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 private:
     DataBase data_base_;
@@ -106,39 +161,5 @@ private:
     QCheckBox *cb_enable_filtering_;
 
 
-    void showEvent(QShowEvent *);
-
-    //show widgets
-    void showCollectionFeeds();
-    void showFilters();
-    void showNotifications();
-    void showProxyConnection();
-    //hide widgets
-    void hideCollectionFeeds();
-    void hideFilters();
-    void hideNotifications();
-    void hideProxyConnection();
-
-    void resizeEvent(QResizeEvent *event);
-    void addItem(QString name);
-    void fillCollectListView();
-    void fillFilterListView();
-    int addStringToWatchList(QString str);
-    void fillViewListView();
-    int addStringToViewList(QString str);
-    void rssTableUpdate();
-    void collect_feedsTableUpdate();
-    void removeDataFromRSSTable(QString site_name, bool all_data);
-    void removeDataFromCollectFeeds(QString site_name);
-    void insertRowToRSSTable(QString site_name, QString url, QString version);
-    void insertRowToFiltersTable(QString filter_name);
-    void removeDataFromFilters(QString filter_name, bool all_data);
-    int addStringToFilterList(QString cur_text);
-    void returnModifedString(QString &str);
-    void updateFiltersTable();
-    void loadOptions();
-    void saveOptions();
-    void updateCollectFeedListView();
-    int cf_label_search_width();
 };
 #endif // OPTIONSWINDOW_H
