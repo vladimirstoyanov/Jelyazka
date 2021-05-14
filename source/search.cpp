b5d7032e55ecea4de43c8d2aa1d11dd23595c841
vladimirstoyanov <firojjsci@gmail.com>
2021-05-14 22:54:07 +0300

    Modified unit test's makefile to build qt source code

--- source/search.cpp
+++ source/search.cpp
@@ -2,12 +2,12 @@
     search.cpp
     Jelyazka RSS/RDF reader
     Copyright (C) 2021 Vladimir Stoyanov
-    
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
---
+++ unit_test/search_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <QString>
+#include "search.h"
+
+TEST(Search, searchBefore)
+{
+    Search s;
+    /*
+    QString text = "This is an example.";
+    QString substing = "an";
+    int index = 0;
+
+    s.searchBefore (text, substing, &index);
+    EXPECT_EQ(index, 7);
+    */
+}
